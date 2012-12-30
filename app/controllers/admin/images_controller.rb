2cdb43b6b17b0338904336d6f3cd04fd22aa3ec0
Jerel Miller <jermille@mines.edu>
2012-12-29 20:48:27 -0700

    Got images editing and deleting

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ImagesController < Admin::AdminController
 
   def index
-    @images = Image.all
+    @images = Image.order('created_at desc')
   end
 
   def new
@@ -18,6 +18,27 @@ class Admin::ImagesController < Admin::AdminController
     else
       flash[:error] = @image.errors.full_messages.join('<br>')
       redirect_to new_admin_image_path
+    end
+  end
+
+  def update
+    @image = Image.find(params[:id])
+    @image.update_attributes(params[:image])
+
+    respond_to do |format|
+      format.html { redirect_to admin_images_path }
+      format.js { render json: @image, status: :accepted }
+    end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.image.destroy
+    @image.destroy
+
+    respond_to do |format|
+      format.html { redirect_to admin_images_path }
+      format.js { render json: '', status: :ok }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Wedding::Application.routes.draw do
 
   get 'login' => 'sessions#new'
   get 'logout' => 'sessions#destroy'
-  resources :sessions, :only => :create
+  resources :sessions, only: :create
 
 
   root to: 'home#index'
 
   namespace :admin do
-    resource :users, :only => [:edit, :update]
-    resources :images
+    resource :users, only: [:edit, :update]
+    resources :images, except: [:edit, :show]
     root :to => 'admin#index'
   end
 
