fcf5f773b7f6cf8eb43b710f43ae64eb8b88bb5c
Jerel Miller <jermille@mines.edu>
2013-01-05 22:38:53 -0700

    Adding logo image, new font, and our story image. Also changing styles to match and deleting unneeded images

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,9 @@ module Wedding
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
+    # Don't bootstrap application assets
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
