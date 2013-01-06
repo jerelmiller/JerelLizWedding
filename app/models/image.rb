6eba2a00f2aabcb65558e664c0c92939c3995bc8
Jerel Miller <jermille@mines.edu>
2013-01-05 20:49:15 -0700

    Adding attr_accessor for image

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,11 @@ class Image < ActiveRecord::Base
   belongs_to :user
 
   attr_accessible :title, :image
+  attr_accessor :image_file_name, 
+    :image_content_type, 
+    :image_file_size, 
+    :image_updated_at
+    
   has_attached_file :image, styles: { regular: ["500x500>", :png], thumb: ["200x200>", :png], large: ["700x700>", :png] },
     url: "/images/:id/:style/:hash.:extension",
     hash_secret: 'jerelAndLiz'
