b0b0880581f697bea206eaeccc6fecef178bcafc
Jerel Miller <jmiller@24tru.com>
2013-03-14 00:01:48 -0600

    Removing some files not needed anymore

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,6 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
-//= require lightbox
-//= require ckeditor/init
 //= require bootstrap
-//= require jquery.isotope.min
 //= require jquery.truncator
 //= require spin
 //= require jquery.spin
@@ -24,6 +9,5 @@
 //= require backbone
 //= require backbone/wedding
 //= require jquery.easing.1.3
-//= require hamlcoffee
 //= require_tree ./backbone/templates
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Wedding::Application.routes.draw do
 
-  mount Ckeditor::Engine => '/ckeditor'
-
   get 'login' => 'sessions#new'
   get 'logout' => 'sessions#destroy'
   resources :sessions, only: :create
 
-
   root to: 'home#index'
-  get 'our-story' => 'home#our_story', as: 'our_story'
-  resources :blogs, only: [:index, :show]
-  resources :images, only: :show
 
   namespace :admin do
     resource :users, only: [:edit, :update]
