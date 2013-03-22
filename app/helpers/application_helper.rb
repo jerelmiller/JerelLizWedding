a960b28bb9413023784ed2d49f48df699b114091
Jerel Miller <jmiller@24tru.com>
2013-03-21 22:44:46 -0600

    Adding registry section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def registry_link
+    'http://www.target.com/GiftRegistrySearchViewCmd?registryType=WD&jsRequest=true&catalogId=10051&status=completePage&cumulativeTime=-1&listId=WWcunRtJPmBK4oQ-0ZTVcA&noOfPings=&registryFirstName=Jerel&langId=-1&segmentGrpName=G&storeId=10151&registryLastName=Miller'
+  end
 end
